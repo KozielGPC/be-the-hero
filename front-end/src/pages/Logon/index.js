acfdfaddd5593266ec6f208c8adedc8e3d843e6a
KozielGPC <gpcgabriel0@gmail.com>
2020-05-23 16:09:28 -0300

    front-end without style

---
+++ front-end/src/pages/Logon/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './styles.css';
+import heroesImg from '../../assets/heroes.png';
+import logoImg from '../../assets/logo.svg';
+import { FiLogIn } from 'react-icons/fi';
+
+export default function Logon (){
+    return (
+        <div className="logon-container">
+            <section className="form">
+            <img src={ logoImg } alt="Logo"/>
+            <form>
+                <h1>Faça seu Logon</h1>
+                <input type="text" placeholder="Sua ID"/>
+                <FiLogIn size= {16} color="#e02041"/>
+                <button type="submit">Entrar</button>
+
+                <a href="/register">Não tenho cadastro</a>
+            </form>
+            </section>
+            <img src={ heroesImg } alt="Heroes"/>
+        </div>
+    );
+}
